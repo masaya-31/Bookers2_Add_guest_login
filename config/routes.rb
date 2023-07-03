3cf1492be8a922bd5743effc67d13df21c031ece
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-07-03 07:15:58 +0000

    [Add]投稿機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get 'home/about' => 'homes#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :books, only: [:index, :show, :edit]
+  resources :books, only: [:index, :create, :show, :edit]
   resources :users, only: [:index, :show, :edit]
 end
