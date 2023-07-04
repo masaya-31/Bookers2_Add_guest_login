d0572b56fdd1f3b3d71f095f5811f069fa9e3a6c
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-07-04 05:58:44 +0000

    [Add]user編集機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :update, :edit]
 end
