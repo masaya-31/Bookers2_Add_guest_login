c93d4cec9e78380e5369aa6217dd5bcbf0d2e343
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-07-04 06:43:05 +0000

    userのshowページの編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path(@book.id)
   end
 
   def show
@@ -19,7 +19,7 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
     book.update(book_params)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    
+    @book = Book.new
   end
+  
 
   def edit
     @user = User.find(params[:id])
