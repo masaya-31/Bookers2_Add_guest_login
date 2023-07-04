8c6abe6a1c60cbb501f387e226e4b76eaeaa92bf
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-07-04 01:06:31 +0000

    [Add]showページの作成,削除機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,13 +9,20 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to root_path
+    redirect_to books_path
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
