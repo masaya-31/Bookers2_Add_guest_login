ac6f26ad21bbc6f9f0d0fa636121c152782bcddd
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-07-04 08:15:49 +0000

    [Add]viewページの編集(リンク追加、記述追加等)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   def index
+    @user = User.find(current_user.id)
     @books = Book.all
 
     @book = Book.new
@@ -14,6 +15,9 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = User.find(@book.user_id)
+
+    @newbook =Book.new
   end
 
   def edit
