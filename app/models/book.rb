fd1f2c6f9c2af7d755d08859107545daf8e70fcb
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-07-06 07:11:41 +0000

    [Add]バリデーションの項目追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,5 +3,7 @@ class Book < ApplicationRecord
   has_one_attached :profile_image
 
   validates :title, presence: true
-  validates :body, presence: true
+  validates :body,
+    presence: true,
+    length: { maximum: 200 }
 end
